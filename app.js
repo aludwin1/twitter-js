e22fda0c08b4b2663d58f622592970f92eba97dd
Aaron Ludwin <aludwin1@binghamton.edu>
2016-07-23 15:21:12 -0400

    forms and filter by name added

--- app.js
+++ app.js
@@ -2,15 +2,46 @@ var express = require('express');
 var morgan = require('morgan');
 var routes = require('./routes/');
 var swig = require('swig');
+var tweetBank = require('./tweetBank');
+var bodyParser = require('body-parser');
 var app = express();
 
-app.use(morgan(':method :url :status'));
-app.use(express.static('public'));
+
+app.use(bodyParser.urlencoded());
+app.use(routes);
+
 
 app.engine('html', swig.renderFile);
 app.set('view engine', 'html');
 app.set('views', __dirname + '/views');
+swig.setDefaults({ cache: false });
 
-app.get('/', routes);
+app.use(morgan(':method :url :status'));
+
+app.use(express.static('public'));
+
+app.use(function (req, res, next) {
+   console.log(req.method + " " + req.originalUrl + " " + res.statusCode);
+   next();
+});
+
+app.use('/special/', function (req, res, next) {
+   console.log("You reached the special area.");
+   next();
+});
+
+app.get('/', function(req, res){
+
+	res.render('index', tweetBank.list());
+});
+
+
+
+app.get('/', function(req, res){
+
+	res.render('index', tweetBank.list());
+});
 
 app.listen(3000);
+
+
--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,16 @@ var router = express.Router();
 // could use one line instead: var router = require('express').Router();
 var tweetBank = require('../tweetBank');
 
-router.get('/', function (req, res) {
+
+router.get('/', function (req, res, next) {
   var tweets = tweetBank.list();
-  res.render( 'index', { title: 'Twitter.js', tweets: tweets } );
+  res.render( 'index', { title: 'Twitter.js', tweets: tweets, showForm: true } );
+});
+
+router.get('/users/:name', function (req, res) {
+  var name = req.params.name;
+  var tweets = tweetBank.find({'name': name});
+  res.render( 'index', { title: 'Twitter.js', tweets: tweets} );
 });
 
 module.exports = router;
