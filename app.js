84a9c1d75a4d5d17c15661d543daa49a6190ad8a
Aaron Ludwin <aludwin1@binghamton.edu>
2016-07-23 14:19:45 -0400

    styled

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 var express = require('express');
 var morgan = require('morgan');
+var routes = require('./routes/');
+var swig = require('swig');
 var app = express();
 
 app.use(morgan(':method :url :status'));
+app.use(express.static('public'));
+
+app.engine('html', swig.renderFile);
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
+
+app.get('/', routes);
 
 app.listen(3000);
